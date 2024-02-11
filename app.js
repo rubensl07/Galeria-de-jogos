c9fdfd82999151e5a114da29b8f992dd9d3305fe
Rubens <rubensluizlobo@gmail.com>
2024-02-11 12:27:45 -0300

    commit

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
 'use strict'
 
-const infos = [
+let infos = [
     {
+        id: 1,
         titulo: "resident evil 2",
         genero: "Survivar horror, adventure, puzzle, first person shooter",
         fundo: "0_a0efVrqAXXeL8uZC.gif",
-        fonte: "'Courier New', Courier, monospace"
+        fonte: 1
     },
     {
+        id: 2,
         titulo: "horizon chase turbo",
         genero: "Indie, racing, action, casual, sports",
         fundo: "213804.gif",
-        fonte: "'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif"
+        fonte: 2
     },
     {
+        id: 3,
         titulo: "celeste",
         genero: "Platform, adventure",
         fundo: "celeste.gif",
-        fonte: "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif"
+        fonte: 3
     },
     {
+        id: 4,
         titulo: "gta v",
         uppercase_title: true,
         genero: "Third person shooter, action, adventure, non-linear",
         fundo: "35a1dca8d52b5583da0773e8a5b01fd6.gif",
-        fonte: "fantasy",
+        fonte: 4
     },
-    {
-        titulo: "Papa's Pizzaria",
-        genero: "Cullinary",
-        fundo: "papa.gif",
-        fonte: "cursive"
-    }
-    
 ]
+
+infos.push({
+    id: 5,
+    titulo: "Papa's Pizzaria",
+    genero: "Cullinary",
+    fundo: "papa.gif",
+    fonte: 5
+},
+{
+    id: 6,
+    titulo: "Life Is Strange",
+    genero: "Action, Interactive Film",
+    fundo: "lis.gif",
+    fonte: 7
+}
+)
 
 function criarCard(infos) {
     const containerGeral = document.getElementById('container')
@@ -43,7 +56,31 @@ function criarCard(infos) {
     jogo.id = 'teste'
     jogo.classList.add('jogo')
     jogo.style.backgroundImage = `url(./content/${infos.fundo})`
-    jogo.style.fontFamily = infos.fonte
+    if(infos.fonte==0){
+        jogo.classList.add('arial')
+    }
+    if(infos.fonte==1){
+        jogo.classList.add('courier')
+    }
+    if(infos.fonte==2){
+        jogo.classList.add('franklin')
+    }
+    if(infos.fonte==3){
+        jogo.classList.add('trebuchet')
+    }
+    if(infos.fonte==4){
+        jogo.classList.add('fantasy')
+    }
+    if(infos.fonte==5){
+        jogo.classList.add('cursive')
+    }
+    if(infos.fonte==6){
+        jogo.classList.add('cedarville')
+    }
+    if(infos.fonte==7){
+        jogo.classList.add('dudu')
+    }
+    
 
     const content = document.createElement('div')
     content.classList.add('content')
@@ -52,7 +89,7 @@ function criarCard(infos) {
 
     const nome = document.createElement('h2')
     nome.textContent = infos.titulo
-    if(infos.uppercase_title){
+    if (infos.uppercase_title) {
         nome.style.textTransform = "uppercase"
     } else {
         nome.style.textTransform = "capitalize"
@@ -61,18 +98,51 @@ function criarCard(infos) {
     const genero = document.createElement('p')
     genero.textContent = infos.genero
 
-    content.replaceChildren(nome,genero)
+    content.replaceChildren(nome, genero)
     jogo.replaceChildren(content)
     containerGeral.appendChild(jogo)
 }
-
+function mensagem(){
+    alert("O upload de imagens não está funcionando no momento :(")
+}
+const uploadContainer = document.getElementById('uploadContainer') 
+uploadContainer.addEventListener('click', mensagem)
 
 infos.forEach(criarCard)
 
-function clicar() {
-    alert('CLICADO')
+const registrarBotao = document.getElementById('registrar')
+const closePanel = document.getElementById('botaoFecharPainel')
+closePanel.addEventListener('click',fecharPainel)
+const painelRegistro = document.getElementById('painelRegistro')
+function abrirPainel() {
+    painelRegistro.style.visibility = 'visible'
 }
 
-function passarMouse(div){
-    alert('sla')
+
+function fecharPainel(){
+    painelRegistro.style.visibility = 'hidden'
 }
+async function registrar() {
+    const tituloDigitado = document.getElementById('titulo').value;
+    const generoDigitado = document.getElementById('genero').value;
+    const fonteEscolhida = document.getElementById('font-select').value
+    const info = {
+        titulo: tituloDigitado,
+        genero: generoDigitado,
+        fundo: "default.gif",
+        fonte: fonteEscolhida
+    }
+
+if(tituloDigitado == "" || generoDigitado == "" || fonteEscolhida == null){
+    alert("Preencha todos os campos")
+} else {
+    console.log(info)
+    painelRegistro.style.visibility = 'hidden'
+    criarCard(info)
+}
+document.getElementById('titulo').textContent == "";
+document.getElementById('titulo').value == "";
+
+}
+const registrarBotaoPainel = document.getElementById('concluirRegistro')
+registrarBotaoPainel.addEventListener('click', registrar)
